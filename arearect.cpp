8197661c9034154bde879aea75baa9b3df2cb226
Meet Alshi <meetalshidevendra@gmail.com>
2025-01-20 00:10:37 +0530

    Add files via upload

---
+++ arearect.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b;
+   
+    cout<<"Enter the length and breadth of rectangle: ";
+    cin>>l>>b;
+    cout<<"Area of rectangle is "<<l*b;
+};
