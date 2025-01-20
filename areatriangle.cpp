3afa67bfe0a9bfd1ee2e52d917c4585c6bbbe2cc
Meet Alshi <meetalshidevendra@gmail.com>
2025-01-21 01:48:44 +0530

    Add files via upload

---
+++ areatriangle.cpp
@@ -0,0 +1,13 @@
+//Area of triangle
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b,h;
+    float area;
+    cout<<"Enter the height and base of triangle:";
+    cin>>b>>h;
+    area=0.5*b*h;
+    cout<<"The area of triangle is "<<area<<endl;
+    return 0;
+};
