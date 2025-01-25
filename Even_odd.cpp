a8e9edff833bceb50bff9268c4b0dfd8580273eb
Meet Alshi <meetalshidevendra@gmail.com>
2025-01-25 23:50:03 +0530

    Add files via upload

---
+++ Even_odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number : ";
+    cin>>num;
+    if (num%2 == 0)
+    {
+        cout<<"The entered no. is even";
+    }
+    else
+    {
+        cout<<"The entered no. is odd";
+    }
+};
