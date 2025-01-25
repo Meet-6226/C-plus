7e8d377cc812174d64c90a93000b9e2a5230ddd6
Meet Alshi <meetalshidevendra@gmail.com>
2025-01-26 00:26:04 +0530

    Add files via upload

---
+++ greatest2no.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter 2 nos.: ";
+    cin>>a>>b;
+    if (a>b)
+    {
+        cout<<a<<" is bigger than "<<b;
+    }
+    else 
+    {
+        cout<<b<<" is greater than "<<a;
+    }
+};
