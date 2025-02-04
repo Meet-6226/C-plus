6a4dce0b2f0f143dfb58766d935ce63c7148d23a
Meet Alshi <meetalshidevendra@gmail.com>
2025-02-04 11:51:53 +0530

    Add files via upload

---
+++ cstyle1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str[20];
+    cout<<"Enter a string: ";
+    cin>>str;
+    cout<<"you entered: "<<str<<endl;
+
+    cout<<"\n Enter another string: ";
+    cin>>str;
+    cout<<"\nYou entered: "<<str<<endl;
+};
