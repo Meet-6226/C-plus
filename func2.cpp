cad1fac6c0fc68aa662bd1fbb401146c589b3206
Meet Alshi <meetalshidevendra@gmail.com>
2025-02-09 00:28:48 +0530

    Add files via upload

---
+++ func2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void display(int n1,float n2)
+{
+    cout<<"The int number is "<<n1<<endl;
+    cout<<"The double number is "<<n2;
+}
+int main()
+{
+    int num1=5;
+    float num2=5.5;
+    display(num1,num2);
+    return 0;
+};
